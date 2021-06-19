dd74730ab29d6d472250db6f4c1174834232c817
Sow Yet <multinaute@gmail.com>
2021-06-19 23:13:39 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "new-tuto-v3-juin21",
+    title: "Tuto gatsby v3",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
