a8f4fdefb466050a9ddf17f5979d60c6da55e587
Sow Yet <multinaute@gmail.com>
2021-07-20 23:48:04 +0200

    finished gatsby tuto part5

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,21 @@ module.exports = {
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
+    "gatsby-remark-images",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-autolink-headers`, `gatsby-remark-prismjs`],
+      },
     },
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,35 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+import * as React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "D MMMM YYYY", locale: "fr")
+          title
+        }
+        id
+        body
       }
     }
   }
-`
+`;
 
-export default BlogPage
+export default BlogPage;
